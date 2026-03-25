3b8a1924ddf63fa8e8339bedd96b232a535055b4
br23aap <br23aap@herts.ac.uk>
2026-03-25 15:05:48 +0000

    Item Class

---
+++ src/main/java/com/itemsharingassociation/Item.java
@@ -0,0 +1,57 @@
+package com.itemsharingassociation;
+
+/**
+ *
+ * @author br191
+ */
+public class Item {
+    private String title;
+    private String language;
+    private Member donatedBy;
+    private Member onLoanTo;
+
+    public Item(String title, String language, String donatedBy) {
+        this.title = title;
+        this.language = language;
+        // donatedBy is String in constructor but Member field - convert/lookup as needed
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public void loanTo(Member borrower) {
+        this.onLoanTo = borrower;
+    }
+
+    public boolean isAvailable() {
+        return onLoanTo == null;
+    }
+
+    public void returnLoan() {
+        this.onLoanTo = null;
+    }
+
+    public Member getDonator() {
+        return donatedBy;
+    }
+
+    public void clearDonator() {
+        this.donatedBy = null;
+    }
+}
+
+
+ 
