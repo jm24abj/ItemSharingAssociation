ad761465f12793ea609a898ab787d330162d5d0d
jb <vagabigben@tutanota.com>
2026-03-26 20:56:59 +0000

    Updated file loading code
    Adding logic for file loading and TODO comments.

--- src/main/java/com/itemsharingassociation/App.java
+++ src/main/java/com/itemsharingassociation/App.java
@@ -17,8 +17,10 @@ public class App extends Application {
     public void start(Stage stage) {
         Collection system = new Collection();
         
+        
         try{
         Scanner fileReader = new Scanner(new File("..\\ItemSharingAssociation\\src\\main\\resources\\input-1.dat"));
+        Member currentMember = null;
         while(fileReader.hasNextLine()){
             String rowOfData [] = fileReader.nextLine().split("|");
             if (rowOfData[0].equals("Member")){
@@ -26,13 +28,45 @@ public class App extends Application {
                 String address = rowOfData[2];
                 String email = rowOfData[3];
                 int donatedQty = Integer.parseInt(rowOfData[4]);
+                currentMember = new Member(name,address,email,donatedQty);
                 // need something that stores the members - arraylist?
+                
             }
+            
             else if(rowOfData[0].equals("Book")){
-        
+                String title = rowOfData[1];
+                String author = rowOfData[2];
+                String isbn = rowOfData[3];
+                String language = rowOfData[4];
+                String borrowerEmail = (rowOfData[5].length() > 5 && !rowOfData[5].isEmpty() ? rowOfData[5] : null);
+              
+                if (currentMember != null){
+                    system.addBook(title, author, currentMember, language, isbn);
+                    
+                    if (borrowerEmail != null){
+                    // TODO
+                    // check if there is a borrower and will set a loan
+                    // need to find the member via email - will need a new method
+                    // and a collection of members
+                    }
+                }
+                
+                else{
+                    // item belongs to a departed member (won't be in the file)
+                    system.addBook(title, author, null, language, isbn);
+                    if (borrowerEmail != null){
+                        // TODO
+                        // search for member via email
+                    }
+                    
+                }
             }
             else if (rowOfData[0].equals("DVD")){
-            
+                String title = rowOfData[1];
+                String language = rowOfData[2];
+                String director = rowOfData[3];
+                String[] audioLanguages = rowOfData[4].split(",");
+                
             }
         }
         
