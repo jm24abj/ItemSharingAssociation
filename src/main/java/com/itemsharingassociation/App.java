2ebd7bdcb6419c59e87b6fc28968769c037e6d2a
jb <vagabigben@tutanota.com>
2026-03-26 21:26:53 +0000

    More code fixes
    Fixing code typos and changed naming.

--- src/main/java/com/itemsharingassociation/App.java
+++ src/main/java/com/itemsharingassociation/App.java
@@ -16,36 +16,38 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         Collection system = new Collection();
-        
-        
+
         try{
         Scanner fileReader = new Scanner(new File("..\\ItemSharingAssociation\\src\\main\\resources\\input-1.dat"));
+        
         Member currentMember = null;
+        
         while(fileReader.hasNextLine()){
-            String rowOfData [] = fileReader.nextLine().split("\\|");
-            if (rowOfData[0].equals("Member")){
-                String name = rowOfData[1];
-                String address = rowOfData[2];
-                String email = rowOfData[3];
-                int donatedQty = Integer.parseInt(rowOfData[4]);
+            String[] lineOfData = fileReader.nextLine().split("\\|");
+            if (lineOfData[0].equals("Member")){
+                String name = lineOfData[1];
+                String address = lineOfData[2];
+                String email = lineOfData[3];
+                int donatedQty = Integer.parseInt(lineOfData[4]);
                 currentMember = new Member(name,address,email,donatedQty);
                 // need something that stores the members - arraylist?
                 
             }
             
-            else if(rowOfData[0].equals("Book")){
-                String title = rowOfData[1];
-                String author = rowOfData[2];
-                String isbn = rowOfData[3];
-                String language = rowOfData[4];
-                String borrowerEmail = (rowOfData.length > 5 && !rowOfData[5].isEmpty() ? rowOfData[5] : null);
+            else if(lineOfData[0].equals("Book")){
+                String title = lineOfData[1];
+                String author = lineOfData[2];
+                String isbn = lineOfData[3];
+                String language = lineOfData[4];
+                String borrowerEmail = (lineOfData.length > 5 && !lineOfData[5].isEmpty()) ? lineOfData[5] : null;
               
                 if (currentMember != null){
                     system.addBook(title, author, currentMember, language, isbn);
                     
                     if (borrowerEmail != null){
-                    // TODO
-                    // check if there is a borrower and will set a loan
+                       Item book = system.getItem(title);
+                    // TODO - loan
+                    // this part will set a loan
                     // need to find the member via email - will need a new method
                     // and a collection of members
                     }
@@ -55,18 +57,18 @@ public class App extends Application {
                     // item belongs to a departed member (won't be in the file)
                     system.addBook(title, author, null, language, isbn);
                     if (borrowerEmail != null){
-                        // TODO
-                        // search for member via email
+                        Item book = system.getItem(title);
+                        // TODO - loan
                     }
                     
                 }
             }
-            else if (rowOfData[0].equals("DVD")){
-                String title = rowOfData[1];
-                String language = rowOfData[2];
-                String director = rowOfData[3];
-                String[] audioLanguages = rowOfData[4].split(",");
-                String borrowerEmail = (rowOfData.length > 5 && !rowOfData[5].isEmpty()? rowOfData[5] : null);
+            else if (lineOfData[0].equals("DVD")){
+                String title = lineOfData[1];
+                String language = lineOfData[2];
+                String director = lineOfData[3];
+                String[] audioLanguages = lineOfData[4].split(",");
+                String borrowerEmail = (lineOfData.length > 5 && !lineOfData[5].isEmpty()) ? lineOfData[5] : null;
                 
                 // to fix white spaces in file
                 for (int i = 0; i < audioLanguages.length; i++)
@@ -76,14 +78,16 @@ public class App extends Application {
                 system.addDVD(title, director, currentMember, language, audioLanguages);
                 
                     if (borrowerEmail != null){
-                        // TODO - need other stuff implemented first
+                        Item dvd = system.getItem(title);
+                        // TODO - loan
                     }
                 }
                 
                 else{
                     system.addDVD(title, director, null, language, audioLanguages);
                     if (borrowerEmail != null){
-                        // TODO
+                        Item dvd = system.getItem(title);
+                        // TODO - loan
                     }
                 
                 }
