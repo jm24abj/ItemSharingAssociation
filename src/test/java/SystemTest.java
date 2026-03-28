25675e00d24d4c6afb97668ca93cf211da619746
jb <vagabigben@tutanota.com>
2026-03-28 23:50:36 +0000

    Added JUnit dependency and created SystemTest
    Created a SystemTest class for JUnit tests. The JUnit dependency has been added to pom.xml

---
+++ src/test/java/SystemTest.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author j
+ */
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SystemTest {
+    
+    @Test
+    public void searchItems(){
+    // TODO
+    // tests searching function
+    
+    }
+    
+    // TODO - other tests
+}
